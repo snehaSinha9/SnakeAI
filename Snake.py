e7b9c05471ac3c72a2bb285a6f94fc54a6269186
sinster9 <ssinha1.314@gmail.com>
2018-12-24 11:49:34 +0000

    initialise

---
+++ Snake.py
@@ -0,0 +1,10 @@
+from numpy import * as np
+
+class Snake:
+    
+    int len = 1
+    int posX = 0 
+    int posY = 0
+    pos = np.array([posX, posY])
+
+    
---
+++ SnakeGUI.py
@@ -0,0 +1,33 @@
+from tkinter import *
+
+class SnakeGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Snake Game")
+        master.grid()
+        
+        self.canvas = Canvas(master, width = 1200, height = 400, bg = "green")
+        self.canvas.pack(side = RIGHT)
+
+        self.label_points = Label(master, text = "POINTS: ")
+        self.label_points.pack(side = TOP)
+
+        self.play_button = Button(master, text="Play", command=self.play)
+        self.play_button.pack(side = LEFT)
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack(side= LEFT)
+
+        self.play()
+
+
+    def play(self):
+        print("Play in progress.")
+
+        self.canvas.delete(ALL)
+
+
+
+root = Tk()
+my_gui = SnakeGUI(root)
+root.mainloop()
