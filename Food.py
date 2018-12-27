707bbc95ce6eb73d8a99500843c19301474f616a
sinster9 <ssinha1.314@gmail.com>
2018-12-27 18:27:04 +0000

    update

--- Food.py
+++ Food.py
@@ -3,13 +3,13 @@ import random
 import math
 import SnakeGUI
 
-class Food(object):
-    __metaclass__ = SnakeGUI
+class Food():
+
     def __init__(self):
         posX = 400 + math.floor(random.randrange(0,49)) *10
         posY = math.floor(random.randrange(0,49)) *10
 
         pos = np.array([posX, posY])
 
-    def show(self, master):
-        self.create_line(0,0,200,100)
+    def show(self):
+        SnakeGUI.my_gui.canvas.create_rectangle(0,0,1,1)
--- SnakeGUI.py
+++ SnakeGUI.py
@@ -8,16 +8,16 @@ class SnakeGUI:
         master.grid()
         
         self.canvas = Canvas(master, width = 1200, height = 400, bg = "green")
-        self.canvas.pack(side = RIGHT)
+        self.canvas.pack(side=RIGHT)
 
         self.label_points = Label(master, text = "POINTS: ")
-        self.label_points.pack(side = TOP)
+        self.label_points.pack(side=TOP)
 
         self.play_button = Button(master, text="Play", command=self.play)
-        self.play_button.pack(side = LEFT)
+        self.play_button.pack(side=LEFT)
 
         self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack(side= LEFT)
+        self.close_button.pack(side=LEFT)
 
         self.play()
 
@@ -26,9 +26,13 @@ class SnakeGUI:
         print("Play in progress.")
 
         self.canvas.delete(ALL)
-        food = Food(self.master)
+
+        
+        food = Food
 
 
-root = Tk()
-my_gui = SnakeGUI(root)
-root.mainloop()
+if __name__ == '__main__':
+    root = Tk()
+    my_gui = SnakeGUI(root)
+    root.mainloop()
+
